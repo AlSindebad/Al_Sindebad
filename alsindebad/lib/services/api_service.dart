10b068ca1a15650520207b4bce43104b8eaadb98
YamamaAbuLiel <abulielyamama9@gmail.com>
2024-05-04 15:50:11 +0300

    app skeleton

---
+++ alsindebad/lib/services/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String _baseUrl = 'https://api.example.com';
+
+  Future<String> fetchData(String endpoint) async {
+    final response = await http.get(Uri.parse('$_baseUrl/$endpoint'));
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Failed to fetch data');
+    }
+  }
+
+// Other methods for interacting with the API...
+}
---
+++ alsindebad/lib/views/screens/qr_scanner.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({super.key});
+
+  @override
+  State<QRScanner> createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
