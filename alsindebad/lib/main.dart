948ac366061d02a1f0c1e5c5066c5d017b472b24
Malakmousa99 <116799883+Malakmousa99@users.noreply.github.com>
2024-05-07 22:57:20 +0300

    new_files_components

--- alsindebad/lib/main.dart
+++ alsindebad/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'views/widgets/categories_view.dart';
 import 'views/widgets/search_component.dart';
+import 'views/widgets/signup.dart';
+import 'views/widgets/signin.dart';
+import 'views/widgets/forget_password.dart';
+import 'views/widgets/new_password.dart';
+import 'views/widgets/card_events.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +13,23 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner:false,
-     //  home: CategoriesComponent(),
-     // home:SearchBarView (),
+      //debugShowCheckedModeBanner: false,
+      //  home: CategoriesComponent(),
+      //home:SignUpForm(),
+      // home:SignInForm(),
+      //  home:ForgetPasswordForm(),
+      //home:NewPasswordForm(),
+      //home:VerificationForm(),
+      //home:EventC}ard(eventName: 'Example Event'),
+   // home: Scaffold(
+   //     body: Center(
+   //       child: EventCard(eventName: 'Event name' , imageUrl:''),
+   //   ),
+   // ),
     );
   }
 }
---
+++ alsindebad/lib/views/widgets/card_events.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+enum ImageShape {
+  rounded
+}
+
+class EventCard extends StatelessWidget {
+  final String eventName;
+  final String imageUrl;
+  final ImageShape imageShape;
+
+  const EventCard({
+    Key? key,
+    required this.eventName,
+    required this.imageUrl,
+    this.imageShape = ImageShape.rounded,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color myColor = const Color(0xFF112466);
+
+    BoxDecoration getDecoration() {
+      switch (imageShape) {
+        case ImageShape.rounded:
+          return BoxDecoration(
+            borderRadius: BorderRadius.circular(31),
+            color: Colors.white,
+            image: DecorationImage(
+              image: NetworkImage(imageUrl),
+            ),
+          );
+      }}
+    return Card(
+      color: myColor,
+      elevation: 8.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(35.0),
+      ),
+      child: Container(
+        padding: EdgeInsets.all(16),
+        child: Row(
+          children: [
+            Container(
+              width: 130,
+              height: 70,
+              decoration: getDecoration(),
+            ),
+
+            SizedBox(width: 27),
+            Expanded(
+              child: Text(
+                eventName,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
