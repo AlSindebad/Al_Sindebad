275e5bc11a2f04d0d34f4e96d3ef4872e18317e3
YamamaAbuLiel <124617912+YamamaAbuLiel@users.noreply.github.com>
2024-05-10 06:15:36 +0300

    Revert "Asa 11 build database"

--- alsindebad/lib/main.dart
+++ alsindebad/lib/main.dart
@@ -1,22 +1,10 @@
-import 'package:firebase_core/firebase_core.dart';
+import 'package:alsindebad/views/screens/home.dart';
+import 'package:alsindebad/views/screens/signup.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  try {
-    await Firebase.initializeApp(
-      options: FirebaseOptions(
-        apiKey: "AIzaSyDzWwJcxToQqCQJHwWQTUt9oOz63VltWgA",
-        databaseURL: "https://console.firebase.google.com/project/sindebad-5d3de/firestore/databases/-default-/data/~2F",
-        projectId: "sindebad-5d3de",
-        messagingSenderId: "987560365634",
-        appId: "sindebad-5d3de",
-      ),
-    );
-    print("Firebase initialized successfully");
-  } catch (e) {
-    print("Error initializing Firebase: $e");
-  }
+void main() {
   runApp(MyApp());
 }
 
@@ -24,44 +12,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Firebase Auth Example',
-      home: MyHomePage(),
-    );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Firebase Auth Example'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => AuthenticationScreen()),
-            );
-          },
-          child: Text('Authenticate'),
-        ),
-      ),
-    );
-  }
-}
 
-class AuthenticationScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Authentication Screen'),
-      ),
-      body: Center(
-        child: Text('Authentication Screen'),
-      ),
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: AppLocalizations.supportedLocales,
+      home: Home(),
     );
   }
 }
