60946fa2797245c66c5da00d37c1af33fbdb165b
danaabdulhaq <danarabdulhaq@gmail.com>
2024-05-14 15:36:16 +0300

    emergancy call screen

--- alsindebad/lib/views/screens/emergancy_call.dart
+++ alsindebad/lib/views/screens/emergancy_call.dart
@@ -1,36 +1,57 @@
 import 'package:flutter/material.dart';
-
 import '../widgets/appBar.dart';
 import '../widgets/tabBar.dart';
 
-class EmergancyCall extends StatefulWidget {
+class EmergancyCall extends StatelessWidget {
   const EmergancyCall({Key? key}) : super(key: key);
 
   @override
-  State<EmergancyCall> createState() => _EmergancyCallState();
-}
-
-class _EmergancyCallState extends State<EmergancyCall> {
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(title:"Emergancy Call"),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: 70),
-            ElevatedButton(
-              onPressed: () {
-                // Implement your emergency call functionality here
-                // For example, you can use the url_launcher package to launch a phone call
-              },
-              child: Text('Call 911'),
+      appBar: CustomAppBar(title: 'Emergency Calls'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print('Police car icon tapped');
+                    },
+                    child: Container(
+                      height: 90,
+                      child: Image.asset('lib/views/icons/police-car.png'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('Phone call icon tapped');
+                    },
+                    child: Container(
+                      height: 90,
+                      child: Image.asset('lib/views/icons/phone-call.png'),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('Call icon tapped');
+                    },
+                    child: Container(
+                      height: 100,
+                      child: Image.asset('lib/views/icons/call.png'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-        Expanded(
-          child: NavigationExample(),)
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 60, // Adjust the height as needed
+            child: NavigationExample(),
+          ),
+        ],
       ),
     );
   }
