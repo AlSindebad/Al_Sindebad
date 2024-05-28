76e098b262daab8202bc395ada83d13e8e3d4416
danaabdulhaq <danarabdulhaq@gmail.com>
2024-05-28 19:45:16 +0300

    Category

--- alsindebad/lib/views/screens/home.dart
+++ alsindebad/lib/views/screens/home.dart
@@ -1,74 +1,68 @@
-
 import 'package:flutter/material.dart';
-import 'package:alsindebad/data/models/place.dart';
-import 'package:alsindebad/views/screens/palce_info.dart';
-import '../../services/place_service.data.dart';
+import 'package:provider/provider.dart';
+import '../../data/models/place.dart';
 import '../widgets/appBar.dart';
 import '../widgets/categories_view.dart';
 import '../widgets/palce_card.dart';
 import '../widgets/search_component.dart';
 import '../widgets/tabBar.dart';
-
+import 'palce_info.dart';
+import '../../viewmodel/place_Category_viewmodel.dart';
 
 class Home extends StatelessWidget {
-  final PlacesService placesService = PlacesService();
-
   Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(title: "Home Page"),
-      body: Column(
-        children: [
-          SearchBarView(),
-          SizedBox(height: 10),
-          Expanded(
-            flex: 2,
-            child: CategoriesComponent(),
-          ),
-          Expanded(
-            flex: 9,
-            child: FutureBuilder(
-              future: placesService.getPlaces(),
-              builder: (context, AsyncSnapshot<List<Places>> snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  List<Places> places = snapshot.data ?? [];
-
-                  return ListView.builder(
-                    itemCount: places.length,
-                    itemBuilder: (context, index) {
-                      Places place = places[index];
-                      return PlaceCard(
-                        title: place.placeName,
-                        location: place.placelocation,
-                        imageUrl: place.placeImage,
-                        averageRating: place.averageRating,
-                        onTap: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => PlaceInfo(
-                                id: place.placeId,
-                                googleMapsUrl: place.locationUrl,
-                              ),
-                            ),
-                          );
-                        },
-                      );
-
-                    },
-                  );
-                }
-              },
+    return ChangeNotifierProvider(
+      create: (context) => PlaceCategory()..data(),
+      child: Scaffold(
+        appBar: CustomAppBar(title: "Home Page"),
+        body: Column(
+          children: [
+            SearchBarView(),
+            SizedBox(height: 10),
+            Expanded(
+              flex: 2,
+              child: CategoriesComponent(),
             ),
-          ),
-          Expanded(child: NavigationExample()),
-        ],
+            Expanded(
+              flex: 9,
+              child: Consumer<PlaceCategory>(
+                builder: (context, viewModel, child) {
+                  if (viewModel.places.isEmpty) {
+                    return Center(child: CircularProgressIndicator());
+                  } else {
+                    return ListView.builder(
+                      itemCount: viewModel.places.length,
+                      itemBuilder: (context, index) {
+                        Places place = viewModel.places[index];
+                        return PlaceCard(
+                          title: place.placeName,
+                          location: place.placelocation,
+                          imageUrl: place.placeImage,
+                          averageRating: place.averageRating,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => PlaceInfo(
+                                  id: place.placeId,
+                                  googleMapsUrl: place.locationUrl,
+                                ),
+                              ),
+                            );
+                          },
+                        );
+                      },
+                    );
+                  }
+                },
+              ),
+            ),
+            Expanded(child: NavigationExample()),
+          ],
+        ),
       ),
     );
   }
