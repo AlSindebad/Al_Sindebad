52b8ccdee4021a8fde93c773758a0966191c977a
YamamaAbuLiel <abulielyamama9@gmail.com>
2024-05-29 14:22:28 +0300

    Update event screen

--- alsindebad/lib/views/screens/event_screen.dart
+++ alsindebad/lib/views/screens/event_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:alsindebad/data/models/event.dart';
 import 'package:alsindebad/views/widgets/app_bar_with_navigate_back.dart';
+import 'package:intl/intl.dart';
 
 class EventScreen extends StatelessWidget {
   final String eventId;
@@ -21,17 +22,12 @@ class EventScreen extends StatelessWidget {
       rethrow;
     }
   }
-
+  String formatDateTime(DateTime dateTime) {
+    return DateFormat('HH:mm:ss').format(dateTime.toLocal());
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(kToolbarHeight + 20), // زيادة الارتفاع بمقدار 20 بكسل
-        child: Padding(
-          padding: const EdgeInsets.only(top: 30.0), // إضافة مسافة علوية لإنزال الـ AppBar للأسفل
-          child: CustomAppBarNavigateBack(title: 'Event'),
-        ),
-      ),
       body: FutureBuilder<Event>(
         future: fetchEvent(),
         builder: (context, snapshot) {
@@ -44,44 +40,54 @@ class EventScreen extends StatelessWidget {
           } else {
             Event event = snapshot.data!;
             print("Event Data: ${event.title}, ${event.imageUrl}"); // Debugging print
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: SingleChildScrollView(
+            return Scaffold(
+              appBar: CustomAppBarNavigateBack(title: event.title),
+              body: SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    SizedBox(height: 50), // إنزال العناصر للأسفل أكثر
+                    event.imageUrl.isNotEmpty ? Image.network(
+                      event.imageUrl,
+                      width:MediaQuery.of(context).size.width,
+                      height: 300,
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Text('Error loading image');
+                      },
+                    )
+                        : Placeholder(fallbackHeight: 200.0, fallbackWidth: double.infinity),
+                    SizedBox(height: 25),
                     Center(
-                      child: event.imageUrl.isNotEmpty
-                          ? Padding(
-                        padding: const EdgeInsets.only(top: 20.0), // إنزال الصورة للأسفل
-                        child: Image.network(
-                          event.imageUrl,
-                          width: 350, // تحديد العرض المطلوب
-                          height: 300, // تحديد الارتفاع المطلوب
-                          fit: BoxFit.cover, // لضبط حجم الصورة داخل الأبعاد المحددة
-                          errorBuilder: (context, error, stackTrace) {
-                            return Text('Error loading image');
-                          },
-                        ),
-                      )
-                          : Placeholder(fallbackHeight: 200.0, fallbackWidth: double.infinity),
-                    ),
-                    SizedBox(height: 25), // إضافة مسافة بعد الصورة
-                    Text(
-                      event.title,
-                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                      child: Text(
+                        event.title,
+                        style: TextStyle(fontSize:28, fontWeight: FontWeight.bold ,color: Color(0xFF112466)),
+                      ),
                     ),
                     SizedBox(height: 8),
-                    Text(
-                      'Date: ${event.date.toLocal().toIso8601String().split('T').first}',
-                      style: TextStyle(fontSize: 16),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        event.description,
+                        style: TextStyle(fontSize: 20 ,color: Color(0xFF112466)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Time: ${formatDateTime(event.date)}',
+                        style: TextStyle(fontSize: 20 , color: Color(0xFF112466)),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Date: ${event.date.toLocal().toIso8601String().split('T').first}',
+                        style: TextStyle(fontSize: 20, color: Color(0xFF112466)),
+                      ),
                     ),
                     SizedBox(height: 16),
-                    Text(
-                      event.description,
-                      style: TextStyle(fontSize: 16),
-                    ),
+
+
                   ],
                 ),
               ),
